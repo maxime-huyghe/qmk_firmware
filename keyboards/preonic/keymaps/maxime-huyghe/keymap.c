1ca84282b8c3c3692a525c15442904f1e90bd83c
maxime-huyghe <maxime.huyghe@etu.u-pec.fr>
2021-10-02 19:08:07 +0200

    move game layer toggle to the top-right key

--- keyboards/preonic/keymaps/maxime-huyghe/keymap.c
+++ keyboards/preonic/keymaps/maxime-huyghe/keymap.c
@@ -43,11 +43,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 [GAME] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    TG(GAME),
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,      KC_M,    KC_COMM, KC_UP,   KC_SLSH, KC_RSFT,
-  KC_LCTL, KC_DOT,  KC_LALT, KC_LGUI, KC_SPC,  MO(LOWER), MO(RAISE), KC_ENT,  KC_LEFT, KC_DOWN, KC_RGHT, TG(GAME)
+  KC_LCTL, _______, KC_LALT, KC_LGUI, KC_SPC,  MO(LOWER), MO(RAISE), KC_ENT,  KC_LEFT, KC_DOWN, KC_RGHT, KC_DOT
 ),
 
 
@@ -70,11 +70,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 [BOTH] = LAYOUT_preonic_grid(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11, KC_F12,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11, TG(GAME),
   xxxxxxx, RESET,   DEBUG,   xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, TERM_ON, TERM_OFF, xxxxxxx, xxxxxxx, xxxxxxx,
   xxxxxxx, xxxxxxx, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,
   xxxxxxx, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,
-  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, TG(GAME)
+  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_F12
 ),
 
 
