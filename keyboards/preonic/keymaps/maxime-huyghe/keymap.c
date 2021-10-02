7e84a3b15fc2256774c21fe9a4369b1ac57b3145
maxime-huyghe <maxime.huyghe@etu.u-pec.fr>
2021-10-02 19:08:08 +0200

    changes for 2u spacebar

--- keyboards/preonic/keymaps/maxime-huyghe/keymap.c
+++ keyboards/preonic/keymaps/maxime-huyghe/keymap.c
@@ -34,20 +34,20 @@ enum preonic_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [QWERTY] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,      KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-  KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  KC_ESC,  KC_A,     KC_S,    KC_D,    KC_F,    KC_G,      KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSPO, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,      KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
-  KC_LCTL, MO(RGB_), KC_LALT, KC_LGUI, LWR_SPC, MO(LOWER), MO(RAISE), RSE_ENT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_GRV,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+  KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  KC_ESC,  KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSPO, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
+  KC_LCTL, MO(RGB_), KC_LALT, KC_LGUI, LWR_SPC, LWR_SPC, MO(RAISE), RSE_ENT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 
 [GAME] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    TG(GAME),
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,      KC_M,    KC_COMM, KC_UP,   KC_SLSH, KC_RSFT,
-  KC_LCTL, _______, KC_LALT, KC_LGUI, KC_SPC,  MO(LOWER), MO(RAISE), KC_ENT,  KC_LEFT, KC_DOWN, KC_RGHT, KC_DOT
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    TG(GAME),
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,      KC_M,    KC_COMM, KC_UP,   KC_SLSH, KC_RSFT,
+  KC_LCTL, _______, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC, MO(RAISE), KC_ENT,  KC_LEFT, KC_DOWN, KC_RGHT, KC_DOT
 ),
 
 
